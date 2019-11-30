c8ac5eee3c2153d38c0c1c4a1cf828c62b2d1609
이종성 <wing109@naver.com>
2019-11-30 16:50:04 +0900

    Redux 설치 후 reducer, store, action, dispatch 테스트

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+
+//@Params: state, action
+const reducer = (state, action) => {
+  if(action.type === 'changeState') {
+    return action.payload;
+  }
+  return 'State'; // 초기상태
+}
+
+//Decorator 패턴?
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+console.log(store);
+console.log("createStore: ", store.getState());
+
+//subscribe
+store.subscribe(()=> console.log('subscribe: ', store.getState()));
+
+//action dispatch test
+const action = {
+  type: "changeState", // 약속된 변수명 type
+  payload: "new State"
+}
+
+//==publish
+store.dispatch(action);
+
+console.log("After dispatch: ", store.getState());
+
+//action, state 를 파라미터로 받아 reducer가 state 를 update하고, 변경이 일어났다면 state를 subscriber에게 push한다.
+//reducer(state, action) => if(state != prevState) push(state, toSubscriber)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
